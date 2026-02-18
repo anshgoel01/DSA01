18f847e32348c3d478337de2cde47b06e7cb50de
Anshuman goel <151727993+anshgoel01@users.noreply.github.com>
2026-02-18 18:02:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (84.31%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -12,28 +12,17 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        if (!root) return ans;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int levelSize = q.size();
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-
-                // last node of this level
-                if (i == levelSize - 1) {
-                    ans.push_back(node->val);
-                }
-
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-        }
-        return ans;
+        vector<int> res;
+        recursion(root,0,res);
+        return res;
     }
+    void recursion(TreeNode* root, int level, vector<int> &res)
+    {
+        if(root==NULL) return;
+        if(res.size()==level) res.push_back(root->val);
+        recursion(root->right,level+1,res);
+        recursion(root->left,level+1,res);
+    }
+
+    
 };
