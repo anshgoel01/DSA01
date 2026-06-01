7ab8014da071f96595725865f5136d27db9b852a
Anshuman goel <151727993+anshgoel01@users.noreply.github.com>
2026-06-01 14:09:40 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (21.54%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+
+        sort(cost.begin(), cost.end(), greater<int>());
+
+        int ans = 0;
+
+        for(int i = 0; i < cost.size(); i++) {
+
+            if((i + 1) % 3 == 0)
+                continue;   // free candy
+
+            ans += cost[i];
+        }
+
+        return ans;
+    }
+};
