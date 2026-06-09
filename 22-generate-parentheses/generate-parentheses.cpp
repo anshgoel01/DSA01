41c4aeb0573094194cae627ae940438a1c0f8c3d
Anshuman goel <151727993+anshgoel01@users.noreply.github.com>
2026-06-09 11:42:05 +0530

    Time: 2 ms (81.53%) | Memory: 16.1 MB (24.22%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
-//ansh
-    void solve(string op, int open, int close, vector<string> &ans){
-        if(open == 0 && close == 0){
-            ans.push_back(op);
-            return;
-        }
-        //when count of open and close brackets are same then 
-        //we have only one choice to put open bracket 
-        if(open == close){
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
-        }
-        else if(open == 0){
-            //only choice is to put close brackets 
-            string op1 = op;
-            op1.push_back(')');
-            solve(op1, open, close-1, ans);
-        }
-        else if(close == 0){
-            //only choise is to use open bracket 
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
-        }
-        else{
-            string op1 = op;
-            string op2 = op;
-            op1.push_back('(');
-            op2.push_back(')');
-            solve(op1, open-1, close, ans);
-            solve(op2, open, close-1, ans);
-        }
+vector<string> ans;
+
+void backtrack(string curr, int open, int close, int n){
+    
+    if(curr.size() == 2 * n){
+        ans.push_back(curr);
+        return;
     }
+    if(open < n){
+        backtrack(curr + "(", open + 1, close, n);
+    } 
+    if(close<open){
+        backtrack(curr + ")", open, close + 1, n );
+
+    } 
+}
     vector<string> generateParenthesis(int n) {
-        int open = n;
-        int close = n;
-        vector<string> ans;
-        string op = "";
-        solve(op, open, close, ans);
-        return ans;  
+        
+        backtrack("",0,0,n);
+        return ans;
     }
 };
