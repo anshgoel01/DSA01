2c5858d3ac755ff6e020b9136d7e8b0acea2f1e0
Anshuman goel <151727993+anshgoel01@users.noreply.github.com>
2026-06-10 23:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (60.76%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void backtrack(int index, vector<int>&arr, int target, vector<vector<int>>&ans, vector<int>&ds){
+            if(target == 0){
+                ans.push_back(ds);
+            return;
+            }
+            for(int i = index; i < arr.size(); i++){
+                if(i>index && arr[i] == arr[i-1]) continue;
+                if(arr[i] > target) break;
+            ds.push_back(arr[i]);
+            backtrack(i+1, arr, target-arr[i], ans, ds);
+            ds.pop_back();
+            }
+            
+        
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        backtrack(0,candidates,target,ans,ds);
+
+        return ans;
+    }
+};
