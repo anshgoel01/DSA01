9b524f86234990e4bd66d46263d2891080c3ae17
Anshuman goel <151727993+anshgoel01@users.noreply.github.com>
2026-08-02 14:15:17 +0530

    Time: 4 ms (37.27%) | Memory: 19.8 MB (11.06%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    // int solve(int left, int right, vector<int>& piles, vector<vector<int>>& dp){
+    //     if(left == right){
+    //         return piles[left];
+    //     }
+
+
+
+    // }
+
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+
+        return true;
+
+
+    }
+};
