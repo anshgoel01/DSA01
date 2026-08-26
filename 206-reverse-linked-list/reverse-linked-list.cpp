379819d657d39d868c4729b3887498e6c4ff237a
Anshuman goel <151727993+anshgoel01@users.noreply.github.com>
2026-08-26 10:21:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (72.59%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -13,14 +13,13 @@ public:
     ListNode* reverseList(ListNode* head) {
         ListNode* temp = head;
         ListNode* prev = NULL;
-        while(temp!=NULL){
+
+        while(temp != NULL){
             ListNode* front = temp->next;
             temp->next = prev;
             prev = temp;
             temp = front;
-
         }
-            return prev;
-        
+        return prev;
     }
 };
